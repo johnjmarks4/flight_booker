de57e1382249717bde07539c289140132bff178c
John Marks <johnjmarks4@gmail.com>
2017-10-30 19:54:11 -0500

    Fix ability to make flight and passenger objects through association

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,10 @@ flight1 = ohare.outgoing_flights.create(id: 1, destination_id: 2, duration: 2)
 
 flight2 = jfk.outgoing_flights.create(id: 2, destination_id: 3)
 
-flight3 = dallas_fortworth.outgoing_flights.create(id: 3, destination_id: 1, time: Time.parse("18/01/20").strftime("%Y-%m-%d"), passengers: 3)
+flight3 = dallas_fortworth.outgoing_flights.create(id: 3, destination_id: 1, time: Time.parse("18/01/20").strftime("%Y-%m-%d"))
+
+passenger1 = Passenger.create(id: 1, name: "John", email: "johnjmarks4@gmail.com")
+
+passenger2 = Passenger.create(id: 2, name: "Bill", email: "Bill@yahoo.com")
+
+passenger3 = Passenger.create(id: 3, name: "Ted", email: "Ted@yahoo.com")
